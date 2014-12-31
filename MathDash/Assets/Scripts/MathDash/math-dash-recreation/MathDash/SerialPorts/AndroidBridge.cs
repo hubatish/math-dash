b63e9f96a34c2d6f4a2780ff46b674076b647d7c
hubatish <Zach@Aesop.(none)>
2014-12-31 14:13:36 -0500

    Tried to get serial ports working

---
+++ MathDash/Assets/Scripts/MathDash/math-dash-recreation/MathDash/SerialPorts/AndroidBridge.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AndroidBridge : MonoBehaviour
+{
+
+	AndroidJavaClass androidClass;
+
+    void Start()
+    {
+		AndroidJNI.AttachCurrentThread();
+		androidClass = new AndroidJavaClass("com.DrexelResearch.MathDash.MathDashBridge");
+    }
+
+	public void checkMyIntOnJava(string message){
+		if (message == "READY") {
+			int myInt = androidClass.CallStatic<int>("getMyInt");
+			Debug.Log("My Int: " + myInt);
+		}
+	}
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Pressed left click.");
+			object[] args = new object[] { Random.Range(1,100), this.gameObject.name, "checkMyIntOnJava" };
+			androidClass.CallStatic("callbackToUnityMethod", args);
+
+        }
+    }
+}
---
+++ MathDash/Assets/Scripts/MathDash/math-dash-recreation/MathDash/SerialPorts/AndroidSerialPort.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+//using USBPortDLL;
+
+namespace MathDash
+{
+    public class AndroidSerialPort : MonoBehaviour
+    {
+        public void Start()
+        {
+            
+            //AndroidJavaObject jo = new AndroidJavaObject("");
+            //UsbManager manager = (UsbManager)getSystemService(Context.USB_SERVICE);
+        //    USBPort usbPort = new USBPort();
+          //  Debug.Log(usbPort.HelloWorld());
+        }
+
+    }
+}
